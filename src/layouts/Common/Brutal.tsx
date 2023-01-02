3a926cd963b0afcc1f59ddd9ef5a2adc496939d1
rashed <x.suwaidi@gmail.com>
2023-01-02 16:54:37 +0400

    prep btn

--- src/layouts/Common/Brutal.tsx
+++ src/layouts/Common/Brutal.tsx
@@ -9,13 +9,18 @@ const BrutalBtn = (props: IBrutalBtnProps) => {
     <div className="my-4 h-14 text-center font-ubuntu-mono font-bold">
       <div
         className={
-          `relative z-10 mb-10 w-44 flex-none before:absolute before:top-2 before:left-2 before:h-14 before:w-44 ` +
-          `before:bg-neon-${props.color}`
+          `relative before:absolute before:top-2 before:left-2  ` +
+          `before:h-14 ` +
+          `w-44 before:w-44 ` +
+          `before:bg-neon-${props.color} `
         }
       >
         <a
           href="#"
-          className="absolute inset-0 z-10 h-14 w-44 bg-white object-cover outline outline-1 outline-black"
+          className={
+            'absolute inset-0 bg-white object-cover outline outline-1 outline-black ' +
+            `w-44 h-14`
+          }
         >
           <p className="my-3 px-6">{props.text}</p>
         </a>
